0cdc9359dbc714c6be52c36572e666b93385678f
Admin <you@example.com>
2022-02-08 19:31:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import NavbarComonent from './commpante/Navbar';
+import FooterContainer from './commpante/footer';
+import Main from './commpante/Main';
+
+function App() {
+  return (
+    <>
+      <NavbarComonent/>
+      <Main/>
+      <FooterContainer/>
+ 
+    
+    </>
+
+
+ 
+ 
+ );
+}
+
+export default App;
